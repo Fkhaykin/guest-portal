bd0f1dba29ab64106d3320460030ac9f4fad85c8
Fkhaykin <fkhaykin@gmail.com>
2026-04-25 12:30:28 -0400

    Add 301 redirects from www rental agreement pages to guest portal

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/en/the-summit-lakehouse-rental-agreement",
+        has: [{ type: "host", value: "www.summitlakeside.com" }],
+        destination: "https://guest.summitlakeside.com",
+        permanent: true,
+      },
+      {
+        source: "/en/the-summit-chateau-rental-agreement",
+        has: [{ type: "host", value: "www.summitlakeside.com" }],
+        destination: "https://guest.summitlakeside.com",
+        permanent: true,
+      },
+      {
+        source: "/en/the-summit-cottage-rental-agreement",
+        has: [{ type: "host", value: "www.summitlakeside.com" }],
+        destination: "https://guest.summitlakeside.com",
+        permanent: true,
+      },
+      {
+        source: "/en/the-summit-manor-rental-agreement",
+        has: [{ type: "host", value: "www.summitlakeside.com" }],
+        destination: "https://guest.summitlakeside.com",
+        permanent: true,
+      },
+      {
+        source: "/en/the-summit-chalet-rental-agreement",
+        has: [{ type: "host", value: "www.summitlakeside.com" }],
+        destination: "https://guest.summitlakeside.com",
+        permanent: true,
+      },
+    ];
+  },
   serverExternalPackages: ["pdfkit"],
   outputFileTracingIncludes: {
     "/api/pepoa/*": ["./node_modules/pdfkit/js/data/**/*", "./public/pepoa-header.png", "./public/BML-logo.png"],
